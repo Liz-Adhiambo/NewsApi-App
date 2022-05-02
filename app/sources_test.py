8ad758677f35e3ca18847c3938ff5d3305079cd6
Liz <adhiamboliz3@gmail.com>
2022-05-02 23:05:00 +0300

    Completed function to pass Sources test

--- app/sources_test.py
+++ app/sources_test.py
@@ -11,7 +11,7 @@ class NewsTest(unittest.TestCase):
         '''
         Set up method that will run before every Test
         '''
-        self.new_Sources = Sources()
+        self.new_Sources = Sources("abc-news","ABC News","Your trusted source for breaking news, analysis, exclusive interviews, headlines, and videos at ABCNews.com.","https://abcnews.go.com","general","us")
 
     def test_instance(self):
         self.assertTrue(isinstance(self.new_Sources,Sources))
