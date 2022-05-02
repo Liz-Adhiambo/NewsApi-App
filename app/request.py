5133d4d2d30472e01e4e71e7a05f975e66738a6c
Liz <adhiamboliz3@gmail.com>
2022-05-02 22:31:32 +0300

    Added vlock content in index.html to display articles

--- app/request.py
+++ app/request.py
@@ -1,4 +1,42 @@
 from app import app
+import urllib.request,json
+from .models import news
+
+News = news.News
 
 # Getting api key
 api_key = app.config['NEWS_API_KEY']
+
+# Getting the movie base url
+base_url = app.config['NEWS_BASE_URL']
+
+
+def get_news():
+    get_news_url= base_url.format(api_key)
+    
+    with urllib.request.urlopen(get_news_url)as url:
+        get_news_data=url.read()
+        get_news_response=json.loads(get_news_data)
+        
+        news_results=None
+        
+        if get_news_response['articles']:
+         news_results_list=get_news_response['articles']
+         news_results=process_results(news_results_list)
+    return news_results
+
+def process_results(news_list):
+    news_results=[]
+    for news_result in news_list:
+        author=news_result.get('author')
+        title=news_result.get('title')
+        description=news_result.get('description')
+        url=news_result.get('url')
+        urlToImage=news_result.get('urlToImage')
+        publishedAt=news_result.get('publishedAt')
+        content=news_result.get('content')
+        
+        news_object=News(author,title,description,url,urlToImage,publishedAt,content)
+        news_results.append(news_object)
+        
+    return news_results
--- app/views.py
+++ app/views.py
@@ -1,11 +1,17 @@
-from flask import render_template
+from flask import render_template,url_for
 from app import app
+from .request import get_news
+
 
 # Views
 @app.route('/')
 def index():
+    
+    
+    
+    news=get_news()
 
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+    return render_template('index.html',articles=news)
