d40005ebefca36a6b4c65a27278c1f0951e36dae
Liz <adhiamboliz3@gmail.com>
2022-05-04 11:48:19 +0300

    Deployment

--- app/views.py
+++ app/views.py
@@ -13,7 +13,7 @@ def index():
     news=get_news()
 
     '''
-    View root page function that returns the index page and its data
+    View root page function that returns the index page and its news data
     '''
     return render_template('index.html',articles=news)
 
